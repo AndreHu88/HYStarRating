e86c5a962b36e40d656d06bffeb1762ffd8b229d
leimo <leimo@huyongdeimac.local>
2017-05-25 14:52:04 +0800

    Initial Commit

---
+++ StarRating/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarRating
+//
+//  Created by leimo on 2017/5/25.
+//  Copyright © 2017年 huyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StarRating/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarRating
+//
+//  Created by leimo on 2017/5/25.
+//  Copyright © 2017年 huyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
